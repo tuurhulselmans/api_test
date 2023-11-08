c74cb9542e5e8fbb6e75df69a9e8926f480037a7
Tuur Hulselmans <007tuur@gmail.com>
2023-11-08 16:40:38 +0100

    weer

--- myproject/models.py
+++ myproject/models.py
@@ -4,15 +4,12 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Weather(Base):
+    __tablename__ = "weather"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
+    city = Column(String, index=True)
+    temperature = Column(Float)
 
 
 class Item(Base):
--- myproject/schemas.py
+++ myproject/schemas.py
@@ -1,35 +1,13 @@
 from pydantic import BaseModel
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
+class WeatherCreate(BaseModel):
+    city: str
+    temperature: float
 
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
+class Weather(WeatherCreate):
     id: int
-    owner_id: int
 
     class Config:
         orm_mode = True
 
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
-
-    class Config:
-        orm_mode = True
